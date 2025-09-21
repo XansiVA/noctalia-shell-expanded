f1ad83b4b053dfa606769995d3933e645da319ef
XansiVA <chaoscatsofficial@gmail.com>
2025-09-21 23:02:23 +0000

    Update Noctalia.sh

--- Noctalia.sh
+++ Noctalia.sh
@@ -26,16 +26,24 @@ paru -S --needed --noconfirm \
     quickshell ttf-roboto inter-font gpu-screen-recorder brightnessctl \
     ddcutil cliphist matugen cava wlsunset xdg-desktop-portal \
     visual-studio-code-bin intellij-idea-community-edition \
-    firefox kitty
+    firefox kitty gnome-text-editor gnome-screenshot oh-my-posh \
+    spotify niri xwayland-satellite gnome-keyring corectl \
+    networkmanager vesktop 
 
 # Step 3: Example file copy (customize paths!)
-SRC_FILE="$HOME/Downloads/example.conf"
+SRC_FILE="$HOME/Downloads/example.con"
 DEST_DIR="$HOME/.config/example"
 DEST_FILE="$DEST_DIR/example.conf"
+
+COPY_NIRI="$HOME/noctalia-shell-expanded/config.kdl"
+COPY_FUZZEL=
+COPY_DESKTOP=
+COPY_IMAGE=
+COPY_
 
 echo -e "${GREEN}Copying $SRC_FILE → $DEST_FILE${RESET}"
 mkdir -p "$DEST_DIR"
 cp "$SRC_FILE" "$DEST_FILE"
 
-echo -e "${GREEN}=== Setup complete! 🎉 ===${RESET}"
+echo -e "${GREEN}=== Setup complete! ===${RESET}"
 
