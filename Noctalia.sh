ee259aa42b6d91c8b3ac8ccc05b03f6a6ffdbf8b
XansiVA <chaoscatsofficial@gmail.com>
2025-10-07 18:21:11 -0500

    Update Noctalia.sh

--- Noctalia.sh
+++ Noctalia.sh
@@ -25,25 +25,47 @@ echo -e "${GREEN}Installing required packages...${RESET}"
 paru -S --needed --noconfirm \
     quickshell ttf-roboto inter-font gpu-screen-recorder brightnessctl \
     ddcutil cliphist matugen cava wlsunset xdg-desktop-portal \
-    visual-studio-code-bin intellij-idea-community-edition \
-    firefox kitty gnome-text-editor gnome-screenshot oh-my-posh \
-    spotify niri xwayland-satellite gnome-keyring corectl \
+    kitty gnome-text-editor oh-my-posh \
+    xwayland-satellite gnome-keyring corectl \
     networkmanager vesktop 
 
-# Step 3: Example file copy (customize paths!)
-SRC_FILE="$HOME/Downloads/example.con"
-DEST_DIR="$HOME/.config/example"
-DEST_FILE="$DEST_DIR/example.conf"
+# Step 3: Copy configuration files
+REPO_DIR="$HOME/noctalia-shell-expanded"
 
-COPY_NIRI="$HOME/noctalia-shell-expanded/config.kdl"
-COPY_FUZZEL=
-COPY_DESKTOP=
-COPY_IMAGE=
-COPY_
+# Niri config
+echo -e "${GREEN}Copying Niri configuration...${RESET}"
+NIRI_SRC="$REPO_DIR/config.kdl"
+NIRI_DEST="$HOME/.config/niri/config.kdl"
+mkdir -p "$(dirname "$NIRI_DEST")"
+cp "$NIRI_SRC" "$NIRI_DEST"
 
-echo -e "${GREEN}Copying $SRC_FILE → $DEST_FILE${RESET}"
-mkdir -p "$DEST_DIR"
-cp "$SRC_FILE" "$DEST_FILE"
+# Fuzzel config Noctalia shell already edits this file.
+#echo -e "${GREEN}Copying Fuzzel configuration...${RESET}"
+#FUZZEL_SRC="$REPO_DIR/fuzzel.ini"
+#FUZZEL_DEST="$HOME/.config/fuzzel/fuzzel.ini"
+#mkdir -p "$(dirname "$FUZZEL_DEST")"
+#cp "$FUZZEL_SRC" "$FUZZEL_DEST"
+
+# Desktop file
+echo -e "${GREEN}Copying desktop file...${RESET}"
+DESKTOP_SRC="$REPO_DIR/NoctaliaQS.desktop"
+DESKTOP_DEST="$HOME/.local/share/applications/noctalia.desktop"
+mkdir -p "$(dirname "$DESKTOP_DEST")"
+cp "$DESKTOP_SRC" "$DESKTOP_DEST"
+
+# Kitty config
+echo -e "${GREEN}Copying Kitty configuration...${RESET}"
+KITTY_SRC="$REPO_DIR/kitty.conf"
+KITTY_DEST="$HOME/.config/kitty/kitty.conf"
+mkdir -p "$(dirname "$KITTY_DEST")"
+cp "$KITTY_SRC" "$KITTY_DEST"
+
+# Bashrc
+echo -e "${GREEN}Replacing .bashrc...${RESET}"
+BASHRC_SRC="$REPO_DIR/.bashrc"
+BASHRC_DEST="$HOME/.bashrc"
+rm -f "$BASHRC_DEST"
+cp "$BASHRC_SRC" "$BASHRC_DEST"
 
 echo -e "${GREEN}=== Setup complete! ===${RESET}"
-
+sleep 1
